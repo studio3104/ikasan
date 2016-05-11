dbbf14348df436a1f1d939c8892d0d469ba8fd43
studio3104 <studio3104.com@gmail.com>
2016-05-11 16:58:18 +0900

    adds concrete implementations to Restrictor

--- lib/ikasan/restrictor.rb
+++ lib/ikasan/restrictor.rb
@@ -1,13 +1,21 @@
 module Ikasan
   class Restrictor
     def initialize(message_count, duration)
+      @message_count = message_count
+      @duration = duration
+      @counter = {}
     end
 
     def increase_sent_count(room)
+      @counter[room] ||= []
+      @counter[room] << Time.now.to_i
     end
 
     def sendable?(room)
-      true
+      return true if @counter[room].nil? || @counter[room].empty?
+      limit = Time.now.to_i - @duration
+      @counter[room] = @counter[room].partition { |t| t >= limit }
+      @counter[room].length <= @message_count
     end
   end
 end
